e7b929baa627e01119d83717bbb20f3652d57e4f
McKay Ririe <mrir2917@740s-pl-214.local>
2016-05-04 09:04:12 -0600

    Don't remember what I changed
    For some reason its red and being a bum.

--- Debug/src/Model/CTECArray.d
+++
@@ -1,8 +0,0 @@
-src/Model/CTECArray.d: ../src/Model/CTECArray.cpp \
-  ../src/Model/CTECArray.h ../src/Model/ArrayNode.h ../src/Model/Node.h
-
-../src/Model/CTECArray.h:
-
-../src/Model/ArrayNode.h:
-
-../src/Model/Node.h:
--- Debug/src/Model/Node.d
+++
@@ -1,3 +0,0 @@
-src/Model/Node.d: ../src/Model/Node.cpp ../src/Model/Node.h
-
-../src/Model/Node.h:
